cb5dfa3dcf9fe9dbae5a2125315a4ebb05883cac
AlbaSanchez9 <aalbasaanz@gmail.com>
2026-03-11 18:34:17 +0100

    Listas todas las mecánicas básicas de VR

---
+++ Assets/Scripts/DebugGrab.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DebugGrab : MonoBehaviour
+{
+    public void OnGrab()
+    {
+        Debug.Log("¡SELECT ENTERED DETECTADO! - El objeto está siendo agarrado");
+    }
+}
